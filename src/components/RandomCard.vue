72edd143868306cf570472aeadeaa30b963175b9
chi2015 <alliluya@yandex.ru>
2018-06-19 15:33:26 +0300

    add RandomCard component

---
+++ src/components/RandomCard.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="main">
+    <div v-bind:class="[cardClass, cards[card]]"/>
+  </div>
+</template>
+
+<script>
+    const Chance = require('chance');
+
+    // Instantiate Chance so it can be used
+    const chance = new Chance();
+
+  export default {
+    data() {
+      return {
+        cards: ['kaluga', 
+                'kazan', 
+                'ufa', 
+                'rostov', 
+                'nizhniy', 
+                'perm',
+                'tomsk',
+                'krasnodar',
+                'archangelsk',
+                'chelyabinsk',
+                'novosibirsk',
+                'vodoprovod',
+                'electro',
+                'stavropol',
+                'rizhskayazd',
+                'belgorod',
+                'leninzd',
+                'kurskzd',
+                'kazanzd',
+                'habarovsk',
+                'omsk',
+                'ekaterinburg',
+                'samara',
+                'saint',
+                'volgograd',
+                'moscow',
+                'tumen',
+                'vladivostok'
+                ],
+        card : 0,
+        shuffling : false
+      };
+    },
+    computed: {
+        cardClass() {
+            if (this.card >= 0 && this.card < 10) return 'monopoly-card1';
+            if (this.card >= 10 && this.card < 20) return 'monopoly-card2';
+            if (this.card >= 20 && this.card < 28) return 'monopoly-card3';
+            return 'monopoly-card1';
+        }
+    },
+    mounted() {
+      //  this.cards.sort(() => Math.random() - 0.5);
+        var changeCardInterval = setInterval(() => {this.card = chance.integer({ min: 0, max: this.cards.length -1  })}, 300);
+        setTimeout(() => {clearInterval(changeCardInterval);}, 5000);
+    },
+    methods: {
+      pay() {
+        if (this.minPayDone) return;
+        if (this.payVal < this.minimalPay) { this.errPay = true; return; }
+        this.errPay = false;
+        this.credit -= this.payVal;
+        this.minPayDone = true;
+        this.errPayDone = false;
+        if (this.credit <= 0) { this.credit = 0; this.creditClear = true; }
+      },
+      addCredit() {
+        if (this.gotCredit) return;
+        if (this.monthsLeft < 12) { this.errGet = true; return; }
+        this.credit += this.creditVal;
+        this.gotCredit = true;
+        this.creditClear = false;
+      },
+      nextMonthUsual() {
+        this.nextMonth(false);
+      },
+      nextMonthForce() {
+        this.nextMonth(true);
+      },
+      nextMonth(force) {
+        if (!force) {
+          if (this.monthsLeft < 12 && !this.minPayDone) { this.errPayDone = true; return; }
+        }
+        this.errPayDone = false;
+        this.monthsLeft -= 1;
+        this.credit = Math.ceil((this.credit * (100 + this.percent)) / 100);
+        this.errGet = false;
+        this.minPayDone = false;
+        this.gotCredit = false;
+        this.payVal = this.minimalPay;
+      },
+      finishGame() {
+        this.gameOver = true;
+      },
+      reset() {
+        this.monthsLeft = 24;
+        this.credit = 1000;
+        this.payVal = 0;
+        this.errPayDone = false;
+        this.errPay = false;
+        this.errGet = false;
+        this.gameOver = false;
+        this.minPayDone = false;
+        this.creditClear = false;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+.main {
+  margin: 0 auto;
+  background-color: #ffffff;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  max-width: 660px;
+}
+
+.monopoly-card1 {
+    background-image: url("../../static/monopoly-cards1.jpg");
+}
+
+.monopoly-card2 {
+    background-image: url("../../static/monopoly-cards2.jpg");
+}
+
+.monopoly-card3 {
+    background-image: url("../../static/monopoly-cards3.jpg");
+}
+
+.monopoly-card1, .monopoly-card2, .monopoly-card3 {
+    margin-top: 40px;
+    width: 325px;
+    height: 500px;
+}
+
+.monopoly-card1.kaluga, .monopoly-card2.novosibirsk {
+    background-position: -100px 0;
+}
+.monopoly-card1.kazan, .monopoly-card2.vodoprovod, .monopoly-card3.omsk {
+    background-position: -425px 0;
+}
+.monopoly-card1.ufa, .monopoly-card2.electro, .monopoly-card3.ekaterinburg {
+    background-position: -750px 0;
+}
+.monopoly-card1.rostov, .monopoly-card2.stavropol, .monopoly-card3.samara {
+    background-position: -1075px 0;
+}
+.monopoly-card1.nizhniy, .monopoly-card2.rizhskayazd, .monopoly-card3.saint {
+    background-position: -1400px 0;
+}
+.monopoly-card1.perm, .monopoly-card2.belgorod {
+    background-position: -100px -500px;
+}
+.monopoly-card1.tomsk, .monopoly-card2.leninzd, .monopoly-card3.volgograd {
+    background-position: -425px -500px;
+}
+.monopoly-card1.archangelsk, .monopoly-card2.kurskzd, .monopoly-card3.moscow {
+    background-position: -750px -500px;
+}
+.monopoly-card1.krasnodar, .monopoly-card2.kazanzd, .monopoly-card3.tumen {
+    background-position: -1075px -500px;
+}   
+.monopoly-card1.chelyabinsk, .monopoly-card2.habarovsk, .monopole-card3.vladivostok {
+    background-position: -1400px -500px;
+}
+</style>
